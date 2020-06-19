9932fe6e5be08a3465415259653cad3ba71b6655
vineelavinny <rajulapati.vineela@gmail.com>
2020-06-19 15:00:10 +0530

    Add files via upload

---
+++ app/Http/Controllers/QuestionnaireController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QuestionnaireController extends Controller
+{
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
+    public function create(){
+        return view('questionnaire.create');
+    }
+     
+    public function store(){
+        $data=Request()->validate([
+            'title'=>'required',
+            'purpose'=>'required',
+        ]);
+        //$data['user-id']=auth()->user()->id;
+        //$questionnaire=\App\Questionnaire::create($data);
+        $questionnaire=auth()->user()->questionnaries()->create($data);
+        return redirect('/questionnaires/'.$questionnaire->id);
+    }
+
+    public function show(\App\Questionnaire $questionnaire){
+        return view('questionnaire.show',compact('questionnaire'));
+    }
+}

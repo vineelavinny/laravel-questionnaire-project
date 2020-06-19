8a24d23404aebee0f960df6ddc17ebc7fd955d5c
vineelavinny <rajulapati.vineela@gmail.com>
2020-06-19 15:03:03 +0530

    Add files via upload

---
+++ storage/framework/views/1affa6dc7f076f0438feaff28e07952039812125.php
@@ -0,0 +1,19 @@
+
+
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"><?php echo e($questionnaire->title); ?></div>
+
+                <div class="card-body">
+                
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\questionnaire\resources\views/questionnaire/show.blade.php ENDPATH**/ ?>
